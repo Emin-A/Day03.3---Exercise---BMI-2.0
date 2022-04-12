ff37fa0aff60f68965d7e069a8f34e9ef9a811b5
Emin Avdovic <72197800+Emin-A@users.noreply.github.com>
2022-04-12 17:41:15 +0000

    Still fixing!

--- main.py
+++ main.py
@@ -2,3 +2,11 @@
 height = float(input("Enter your height in m: "))
 weight = float(input("Enter your weight in kg: "))
 
+# underweght, normal_weight, overweight, obese, clinically obese
+body_mass_index = weight / height**2 
+# example: 85 / 1,75 * 1,75 = 27,75 BMI
+if body_mass_index <= 18.5:
+  print("Your BMI is body_mass_index. You are underweight!")
+else:
+  print("Your BMI is body_mass_index. You have normal weight!")
+  
